572e6acbaf193bffb3eab93f34bdf7783d6f43eb
Natalie Halloran <natalie.halloran@yahoo.com>
2021-06-02 00:20:09 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,25 @@
+function theBeatlesPlay(musicians,instruments) {
+  var m = [];
+  for(let i = 0; i < m.length - 1; i++) {
+    m.push(`${musicians[i]} plays ${instruments[i]}`);
+  }
+return m
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
